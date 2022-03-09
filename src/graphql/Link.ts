60ad8bb1af00a863bd8d8bd6a6fe02a5a9d9d069
John <ashton.basile@gmail.com>
2022-03-09 14:37:02 +0100

    Completed the Voting and Custom GraphQL Scalars chapter.

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -7,14 +7,23 @@ export const Link = objectType({
 		t.nonNull.int("id"); 
 		t.nonNull.string("description"); 
 		t.nonNull.string("url"); 
-		t.field("postedBy", {   // 1
+		t.nonNull.dateTime("createdAt");
+		t.field("postedBy", {   
 			type: "User",
-			resolve(parent, args, context) {  // 2
+			resolve(parent, args, context) {  
 				return context.prisma.link
 					.findUnique({ where: { id: parent.id } })
 					.postedBy();
 			},
-	});
+		});
+		t.nonNull.list.nonNull.field("voters", {  
+			type: "User",
+			resolve(parent, args, context) {
+				return context.prisma.link
+					.findUnique({ where: { id: parent.id } })
+					.voters();
+			}
+		})
 	},
 });
 
