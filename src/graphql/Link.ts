64023dbfd7a1757d0cad7f5b528e6688353c8433
John <ashton.basile@gmail.com>
2022-03-09 10:15:29 +0100

    Made it through the Verifying client side JWT tokens section of the Authentication section.

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -7,6 +7,14 @@ export const Link = objectType({
 		t.nonNull.int("id"); 
 		t.nonNull.string("description"); 
 		t.nonNull.string("url"); 
+		t.field("postedBy", {   // 1
+			type: "User",
+			resolve(parent, args, context) {  // 2
+				return context.prisma.link
+					.findUnique({ where: { id: parent.id } })
+					.postedBy();
+			},
+	});
 	},
 });
 
